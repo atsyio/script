4efaf5112c9772fa91ec27402d2f1340f1cacb07
atsyio <220265675+atsyio@users.noreply.github.com>
2025-09-08 08:05:06 +0300

    add exclude arg to parseAnimals

--- parser.lua
+++ parser.lua
@@ -25,32 +25,34 @@ function Parser.parseBase()
     return ""
 end
 
-function Parser.parseAnimals()
+function Parser.parseAnimals(exclude)
     local animals = {}
 
     for _, plot in ipairs(workspace.Plots:GetChildren()) do
-        local animalPodiums = plot:FindFirstChild("AnimalPodiums")
-        if animalPodiums then
-            for _, podium in ipairs(animalPodiums:GetChildren()) do
-                local podiumBase = podium:FindFirstChild("Base")
-                if podiumBase then
-                    local spawnPoint = podiumBase:FindFirstChild("Spawn")
-                    if spawnPoint then
-                        local attachment = spawnPoint:FindFirstChild("Attachment")
-                        if attachment then
-                            local overheadInfo = attachment:FindFirstChild("AnimalOverhead")
-                            if overheadInfo then
-                                local animal = {
-                                    Name        = overheadInfo:FindFirstChild("DisplayName")    and overheadInfo.DisplayName.Text                       or "Unknown",
-                                    Income      = overheadInfo:FindFirstChild("Generation")     and Parser.parseIncome(overheadInfo.Generation.Text)    or 0,
-                                    Generation  = overheadInfo:FindFirstChild("Generation")     and overheadInfo.Generation.Text                        or "$0/s",
-                                    Rarity      = overheadInfo:FindFirstChild("Rarity")         and overheadInfo.Rarity.Text                            or "Common",
-                                    Stolen      = overheadInfo:FindFirstChild("Stolen")         and overheadInfo.Stolen.Visible                         or false,
-                                    Stand       = podiumBase:FindFirstChild("Decorations")      and podiumBase.Decorations                              or nil,
-                                    Podium      = podium
-                                }
+        if plot.Name ~= exclude then
+            local animalPodiums = plot:FindFirstChild("AnimalPodiums")
+            if animalPodiums then
+                for _, podium in ipairs(animalPodiums:GetChildren()) do
+                    local podiumBase = podium:FindFirstChild("Base")
+                    if podiumBase then
+                        local spawnPoint = podiumBase:FindFirstChild("Spawn")
+                        if spawnPoint then
+                            local attachment = spawnPoint:FindFirstChild("Attachment")
+                            if attachment then
+                                local overheadInfo = attachment:FindFirstChild("AnimalOverhead")
+                                if overheadInfo then
+                                    local animal = {
+                                        Name        = overheadInfo:FindFirstChild("DisplayName")    and overheadInfo.DisplayName.Text                       or "Unknown",
+                                        Income      = overheadInfo:FindFirstChild("Generation")     and Parser.parseIncome(overheadInfo.Generation.Text)    or 0,
+                                        Generation  = overheadInfo:FindFirstChild("Generation")     and overheadInfo.Generation.Text                        or "$0/s",
+                                        Rarity      = overheadInfo:FindFirstChild("Rarity")         and overheadInfo.Rarity.Text                            or "Common",
+                                        Stolen      = overheadInfo:FindFirstChild("Stolen")         and overheadInfo.Stolen.Visible                         or false,
+                                        Stand       = podiumBase:FindFirstChild("Decorations")      and podiumBase.Decorations                              or nil,
+                                        Podium      = podium
+                                    }
 
-                                table.insert(animals, animal)
+                                    table.insert(animals, animal)
+                                end
                             end
                         end
                     end
